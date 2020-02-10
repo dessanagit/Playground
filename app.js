fd6bd1f09b9e7117db1e7a1e73ad5fec80c787ad
Dessana Santos <dessanacms@gmail.com>
2020-02-10 15:55:22 -0400

    EJS skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+// jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const title = "Hello World!";
+
+app.get("/", function(req, res){
+  res.render("index", {titleContent: title});
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
